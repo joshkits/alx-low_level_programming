db0b677fb41633d525d149012115ebeeebd9d1c9
omonuberu joshua <omonuberojoshua@gmail.com>
2023-10-07 03:50:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that print a line
- * Return : 0 (on sucess)
+ *
+ * Return: Always 0 (on success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
